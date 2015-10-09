860e0de0f425c73fe6aa8775e87647a1afac24e2
Keydrain <clintgcooper@gmail.com>
2015-10-09 07:28:16 -0600

    Additional Comments
    Added more comments to the code to better describe functionality.
    RBFN derivative is most likely the problem for RBFN’s (updateWeights =>
    ‘G’).

--- Project2/NN.py
+++ Project2/NN.py
@@ -17,7 +17,10 @@ import time
 import copy
 from operator import sub
 
+# Class for a single node in either network and in input, hidden, or output layer
 class node:
+	# Constructor for node class
+	# Takes in 3 option parameters for a function, a starting value, and a dmax
 	def __init__(self, appFunc = '', value = 0, dmax = 1):
 		self.inputs = []
 		self.weights = []
@@ -42,29 +45,37 @@ class node:
 		self.weights.append(random.random()-0.5)
 		self.historicalWeights.append(0)
 
-	# Returns the set of input nodes for this node
-	def getInputs(self):
-		return self.inputs
-
 	# Add a node as an output to this node
 	def addOutput(self, node):
 		self.outputs.append(node)
-
-	# Return the set of output nodes
-	def getOutputs(self):
-		return self.outputs
 
 	# Set the value of this node without calculating it
 	def setValue(self, value):
 		self.value = value
 
-	# Return this nodes current value
-	def getValue(self):
-		return self.value
-
 	# Set the error of this node without calculating it
 	def setNewError(self, newError):
 		self.error = newError
+
+	# Set the weights to the values provided by the list values
+	def setWeights(self, values):
+		self.weights = values
+
+	# Set the dmax of this node to the newdmax
+	def setDmax(self, newdmax):
+		self.dmax = newdmax
+
+	# Returns the set of input nodes for this node
+	def getInputs(self):
+		return self.inputs
+
+	# Return the set of output nodes
+	def getOutputs(self):
+		return self.outputs
+
+	# Return this nodes current value
+	def getValue(self):
+		return self.value
 
 	# Return the error of this node
 	def getError(self):
@@ -83,71 +94,78 @@ class node:
 	# Return the set of weights for the input nodes and this node
 	def getWeights(self):
 		return self.weights
-
-	# Set the weights to the values provided by the list values
-	def setWeights(self, values):
-		self.weights = values
-
-	# Set the dmax of this node to the newdmax
-	def setDmax(self, newdmax):
-		self.dmax = newdmax
  
  	# Calculate the value of this node. 
  	# This is dependent on the function this node possesses.
+	# summa is the summation of the values of the input nodes multiplied by their weights
 	def calcValue(self): 
 		summa = 0	
 		for x in self.inputs: summa += x.getValue()*self.weights[self.inputs.index(x)]
-		# summa is the summation 
-		if self.func == 'S':	#sigmoid
+		# Sigmoid Function
+		if self.func == 'S':
 			self.value = 1 / (1 + math.exp(-summa))
-		elif self.func == 'B': 	#bias value
+		# Bias Node Function
+		elif self.func == 'B':
 			self.value = 1
-		elif self.func == 'G': 	#guassian 
+		# Gaussian Function
+		elif self.func == 'G':
 			inputVector = []
 			for x in self.inputs:
 				inputVector.append(x.getValue())
 			gaussInput = list(map(sub, inputVector, self.weights[:-1]))
 			width = 2*self.dmax/(2*len(self.inputs[0].getOutputs()))
 			self.value = math.e**((-EuclideanDistance(gaussInput)**2)/width)
-		elif self.func == 'L': 	#linear step
+		# Linear Step Function
+		elif self.func == 'L':
 			if summa > 0: self.value = 1
 			else: self.value = 0
-		elif self.func == 'R':	#summation
+		# Summation Function for RBF
+		elif self.func == 'R':
 			self.value = summa
 		else: self.value = 1
 
+	# Calculate the Error assuming this is a hidden layer node
+	# summa is the summation of the errors from the output nodes multiplied by their weights
 	def calcHiddenError(self):
 		summa = 0
 		for x in self.outputs: summa += x.getError() * x.getWeightForNode(self)
+		# Sigmoid Error
 		if self.func == 'S':
 			self.error = self.value * (1-self.value) * summa
+		# Linear Step Error
 		elif self.func == 'L':
 			self.error = summa 
-			#self.error = self.value 
+		# Gaussian Error (Should not be needed / used)
 		elif self.func == 'G':
 			self.error = 1
+		# Safety
+		else:
+			self.error = 0
 
+	# Calculate the Error assuming this is an output layer node
 	def calcOutputError(self, answer):
-		if self.func == 'S':	
+		# Sigmoid Error
+		if self.func == 'S':
 			self.error = (answer - self.value) * self.value * (1 - self.value)
-		elif self.func == 'L': 
-			#print(answer, self.value)
+		# Linear Step Error and RBF Error (Delta Rule as derivative of the Linear Step Function is 1)
+		elif self.func == 'L' or self.func == 'R': 
 			self.error = (answer - self.value)
-		elif self.func == 'R':
-			self.error = (answer - self.value)
-			#self.error = (answer - self.value)**2 This should be the error, but the values don't converge. 
-		else: self.error = 0
+		# Safety
+		else: 
+			self.error = 0
 
+	# Update all of the input weights for this node
+	# Requires the Learning Rate (LearnRate), Momentum, and current loop (loop) to calculate
 	def updateWeights(self, LearnRate, Momentum, loop):
 		global Bloops
 		DLR = 1 - 1/(Bloops-loop+1) # Linear decreasing relationship
-		#DLR = -((1 + 1/(Bloops ** 5))**(loop ** 10)) + 2 # This is a bit arbitrary... 
+		# Sigmoid, Linear Step, and RBF Output nodes apply a new weight based on their error. 
 		if self.func == 'S' or self.func == 'L' or self.func == 'R':
 			for i in range(len(self.weights)):
 				temp = self.weights[i]
-				self.weights[i] = self.weights[i] + ((1 - Momentum) * max(LearnRate, DLR) * self.error * self.inputs[i].getValue())
-				self.weights[i] = self.weights[i] + (Momentum * (self.weights[i] - self.historicalWeights[i]))
+				self.weights[i] = self.weights[i] + ((1 - Momentum) * max(LearnRate, DLR) * self.error * self.inputs[i].getValue()) + (Momentum * (self.weights[i] - self.historicalWeights[i]))
 				self.historicalWeights[i] = temp
+		# Gaussian nodes apply a new weight based on the derivative of the Gaussian equation (TBD) and their error
 		elif self.func == 'G':
 			for i in range(len(self.weights)):
 				temp = self.weights[i]
@@ -160,8 +178,11 @@ class node:
 				self.weights[i] = self.weights[i] + (Momentum * (self.weights[i] - self.historicalWeights[i]))
 				self.historicalWeights[i] = temp
 
+# A single Neural Network that will approximate a function via an input vector, node arrangement matrix, output vector, answer vector, 
+# learning rate (optional) (0,1], threshold value (optional) (0,∞), momentum value (optional) (0,1]
 class NN:
-	def __init__(self, inputs, arrangement, outputs, answers, learnrate, threshold, momentum):
+	# Constructor for the Neural Network
+	def __init__(self, inputs, arrangement, outputs, answers, learnrate = 0.2, threshold = 1, momentum = 0.5):
 		self.StartingNodes = []
 		self.HiddenNodes = []
 		self.OutputNodes = []
@@ -175,14 +196,8 @@ class NN:
 		self.arrangement = arrangement
 		self.outputs = outputs
 
-	def SetDmax(self, newdmax):
-		self.Dmax = newdmax
-		for i in range(len(self.HiddenNodes)):
-			for j in range(len(self.HiddenNodes[i])):
-				self.HiddenNodes[i][j].setDmax(newdmax)
-
+	# Construct the network from the inputs
 	def ConstructNetwork(self):
-		# Construct the network from the inputs
 		# Make Start Nodes
 		for x in self.inputs:
 			n = node(value = x)
@@ -192,6 +207,7 @@ class NN:
 			temp = []
 			for x in y:
 				if self.arrangement.index(y) == 0:
+					# Gaussian nodes need to keep track of a dmax value
 					if x == 'G': 
 						n = node(appFunc = x, dmax = self.Dmax)
 					else: 
@@ -210,31 +226,39 @@ class NN:
 			self.OutputNodes.append(n)
 		# Network created and ready to function
 
+	# Reset the starting nodes values to values
 	def SetStartingNodesValues(self, values):
 		for i in range(len(self.StartingNodes)):
 			self.StartingNodes[i].setValue(values[i])
 
+	# Reset the answerSet for the NN that is used to train against to values
 	def SetAnswerSetValues(self, values):
 		for i in range(len(self.AnswerSet)):
 			self.AnswerSet = values
 
-	def getAnswerSet(self):
-		return self.AnswerSet
+	# Reset the dmax of all the hidden nodes in the NN
+	def SetDmax(self, newdmax):
+		self.Dmax = newdmax
+		for i in range(len(self.HiddenNodes)):
+			for j in range(len(self.HiddenNodes[i])):
+				self.HiddenNodes[i][j].setDmax(newdmax)
 
+	# Print all the values, errors, and weights contained within this NN
 	def PrintStatus(self):
 		print()
-		#for x in self.StartingNodes:
-		#	print(id(x), 'has starting value:', x.getValue())
-		#for y in self.HiddenNodes:
-		#	for x in y:
-		#		print(id(x), 'has hidden value:', x.getValue())
-		#		print(id(x), 'has hidden error:', x.getError())
-		#		print(id(x), 'had weights:', x.getWeights())
+		for x in self.StartingNodes:
+			print(id(x), 'has starting value:', x.getValue())
+		for y in self.HiddenNodes:
+			for x in y:
+				print(id(x), 'has hidden value:', x.getValue())
+				print(id(x), 'has hidden error:', x.getError())
+				print(id(x), 'had weights:', x.getWeights())
 		for x in self.OutputNodes:
 			print(id(x), 'has output value:', x.getValue(), '~', self.AnswerSet[self.OutputNodes.index(x)])
-			#print(id(x), 'has output error:', x.getError())
-			#print(id(x), 'had weights:', x.getWeights())
+			print(id(x), 'has output error:', x.getError())
+			print(id(x), 'had weights:', x.getWeights())
 
+	# Calculate the answer of the NN
 	def CalculateNNOutputs(self):
 		for i in range(len(self.HiddenNodes)):
 			for j in range(len(self.HiddenNodes[i])):
@@ -242,11 +266,13 @@ class NN:
 		for i in range(len(self.OutputNodes)):
 			self.OutputNodes[i].calcValue()
 
+	# Calculate the error from the output. Should only ever be run after ShouldBackprop() has been run.
 	def CalculateNNErrors(self):
 		for i in range(len(list(reversed(self.HiddenNodes)))):
 			for j in range(len(list(reversed(self.HiddenNodes))[i])):
 				(list(reversed(self.HiddenNodes))[i][j]).calcHiddenError()
 
+	# Returns the set of all the weights contained within the NN
 	def GetNNWeights(self):
 		weightSet = []
 		for i in range(len(self.HiddenNodes)):
@@ -257,6 +283,7 @@ class NN:
 				weightSet.append(x)
 		return weightSet
 
+	# Returns the set of all the weights contained within the NN after removing the bias node's weights
 	def GetNNWeightsTrim(self):
 		weightSet = []
 		for i in range(len(self.HiddenNodes)):
@@ -267,6 +294,7 @@ class NN:
 				weightSet.append(x)
 		return weightSet
 
+	# Sets all of the weights of the network. Mirror function for GetNNWeights
 	def SetNNWeights(self, values):
 		counter = 0
 		for i in range(len(self.HiddenNodes)):
@@ -283,6 +311,7 @@ class NN:
 				counter += 1
 			self.OutputNodes[i].setWeights(temp)
 
+	# Calculate the weights of the NN by forward propagation
 	def UpdateNNWeights(self, loop):
 		for i in range(len(self.HiddenNodes)):
 			for j in range(len(self.HiddenNodes[i])):
@@ -290,12 +319,14 @@ class NN:
 		for i in range(len(self.OutputNodes)): 
 			self.OutputNodes[i].updateWeights(self.LearnRate, self.Momentum, loop)
 
+	# Returns the values of the output nodes
 	def GetNNResults(self):
 		resultSet = []
 		for i in range(len(self.OutputNodes)): 
 			resultSet.append(self.OutputNodes[i].getValue())
 		return resultSet
 
+	# Calculates the error of the output nodes and determines if the results are within the threshold percentage. 
 	def ShouldBackprop(self):
 		backprop = False
 		for i in range(len(self.OutputNodes)):
@@ -306,10 +337,7 @@ class NN:
 		self.converged = backprop
 		return self.converged
 
-def ProcessGroupFunc(instance, loops):
-	instance.CalculateNNErrors()
-	instance.UpdateNNWeights(loops)
-
+# Calculate the Euclidean Distance of the vector. This is mostly for easy of naming in other functions. 
 def EuclideanDistance(vector):
 	#print(vector)
 	return linalg.norm(vector)
@@ -317,18 +345,24 @@ def EuclideanDistance(vector):
 	#Still need to test Linear and Sigmoid with RBFN
 	#Still need to test Linear with FFNN
 
+# Our Main Method that takes in the list of input vectors, the arrangement (topology), the list of output vectors, the list of answer vectors,
+# the NN Learning Rate (learnrate), the threshold percentage (threshold), and the momentum scalar (momentum)
+# Returns the NN that has been trained and is ready for testing. Testing code will be handled in the Handler File.
 def main(inputs, arrangement, outputs, answers, learnrate = 0.5, threshold = 1, momentum = 0):
 	global Bloops
 	Bloops = 100 ** len(inputs)
 	NNinstances = []
 	OrigAnswers = copy.deepcopy(answers)
 
+	# Max and Min of our outputs
 	maxim = 0
 	for x in answers: maxim = max(maxim, max(x))
-
 	minim = 10000
 	for x in answers: minim = min(minim, min(x))
 
+	# Scale our outputs and threshold according to the domain of all our possible answers
+	# New range is between 0.2 and 0.8 thus having a buffer of 0.2 on either side to accommodate an approach from that direction (initial guess)
+	# or the possibility of estimating an answer that exceeds the domain of our training data. 
 	for i in range(len(answers)):
 		for j in range(len(answers[i])):
 			if (maxim == minim): 
@@ -337,6 +371,7 @@ def main(inputs, arrangement, outputs, answers, learnrate = 0.5, threshold = 1, 
 				answers[i][j] = (((answers[i][j] - minim) * (0.8 - 0.2)) / (maxim - minim)) + 0.2
 				threshold = (threshold / (maxim - minim))
 
+	# Initial NN template that is duplicated for each input vector. 
 	baseNN = NN(inputs[0], arrangement, outputs, answers[0], learnrate, threshold, momentum)
 	baseNN.ConstructNetwork()
 
@@ -352,12 +387,14 @@ def main(inputs, arrangement, outputs, answers, learnrate = 0.5, threshold = 1, 
 			temp.append(x)
 		counter += 1
 
-	#print('Cats')
+	# Calculate the max spread between estimated centers. This calculation may need a bit of fine tuning. 
 	dmax = 0
 	for x in cVectors:
 		for y in cVectors:
 			dmax = max((EuclideanDistance(list(map(sub, x, y)))), dmax)
 
+	# Create a copy of the template and set it's inputs and answers to the appropriate vectors.
+	# Then saves this new NN as an instance in NNinstances
 	for i in range(len(inputs)):
 		temp = copy.deepcopy(baseNN)
 		temp.SetDmax(dmax)
@@ -368,34 +405,39 @@ def main(inputs, arrangement, outputs, answers, learnrate = 0.5, threshold = 1, 
 
 	loops = 0
 	while True:
-		#print()
+		# Calculate the outputs of all instances.
 		for i in range(len(NNinstances)): 
 			NNinstances[i].CalculateNNOutputs()
-			#NNinstances[i].PrintStatus()
+		# Calculate the output layer's error and determine if the network needs to backprop.
 		done = True
 		for i in range(len(NNinstances)): 
 			if NNinstances[i].ShouldBackprop(): done = False
+		# Merge all the weights from every instance and average the values, then reset each NN to have these new Weights
 		weightSet = []
 		for i in range(len(NNinstances)): weightSet.append(NNinstances[i].GetNNWeights())
 		weightSet = transpose(weightSet)
 		newWeightSet = []
 		for x in weightSet: newWeightSet.append((sum(x))/len(x))
 		for i in range(len(NNinstances)): NNinstances[i].SetNNWeights(newWeightSet)
+		# Calculate the output layer's error and determine if the network needs to backprop again. 
+		# If at this point, neither test has failed and flipped done to false, we have a valid Weight set for use as a solution.
 		for i in range(len(NNinstances)): 
 			if NNinstances[i].ShouldBackprop(): 
-				#print(i)
 				done = False
-		#print()
 		print("Progress {:2.1%}".format(loops / Bloops), end="\r")
+		# Make sure we have iterated at least 100 times before presenting our solution. Prevents us from being lucky.
 		if (done and (loops >= 100)): break
 		loops += 1
+		# Gets us out of this loop if we have backproped more times than our Max number of loops: Bloops
 		if loops > (Bloops):
 			#print('Reached an iterative bound. Bailing!')
 			break
+		# If we reach this point, then the network needs to fully backprop and update its errors and weights before recalculating its outputs.
 		for i in range(len(NNinstances)):
 			NNinstances[i].CalculateNNErrors()
 			NNinstances[i].UpdateNNWeights(loops)
 
+	# See the output of each NN and how close it thought it got to the function it was learning. 
 	results = answers
 	for i in range(len(NNinstances)):
 		for j in range(len(NNinstances[i].GetNNResults())):
@@ -403,8 +445,10 @@ def main(inputs, arrangement, outputs, answers, learnrate = 0.5, threshold = 1, 
 			else: results[i][j] = (((NNinstances[i].GetNNResults()[j] - 0.2) * (maxim - minim)) / (0.8 - 0.2)) + minim
 	for i in range(len(results)): print(loops, inputs[i], results[i], OrigAnswers[i])
 
+	# Select one of the finished NN's as they should all be the same and call it your final NN. 
 	finalNN = copy.deepcopy(NNinstances[0])
 
+	# Test your original input vectors on the finalNN. Results should be very accurate. 
 	for x in inputs:
 		#print(x)
 		finalNN.SetStartingNodesValues(x)
@@ -416,16 +460,17 @@ def main(inputs, arrangement, outputs, answers, learnrate = 0.5, threshold = 1, 
 	#finalNN.CalculateNNOutputs()
 	#print(loops, [3.5], ((((finalNN.GetNNResults()[0] - 0.2) * (maxim - minim)) / (0.8 - 0.2)) + minim), [12.25])
 
+	# Ready to run tests on this NN
 	return finalNN
 
 if __name__== '__main__':
 	print('Starting some NN tests...\n')
 	
 	#main([[2,3]], [['S','S','S'], ['S', 'S']], ['S'], [[101]], learnrate = 0.5, threshold = 10, momentum = 0.5)
-	main([[2,3], [1,3]], [['S','S','S'], ['S', 'S']], ['S'], [[101], [400]], learnrate = 0.1, threshold = 1, momentum = 0.5)
+	#main([[2,3], [1,3]], [['S','S','S'], ['S', 'S']], ['S'], [[101], [400]], learnrate = 0.1, threshold = 1, momentum = 0.5)
 	#main([[2,3], [1,3], [3,3]], [['S','S','S'], ['S','S']], ['S'], [[101], [400], [3604]], learnrate = 0.5, threshold = 1, momentum = 0.5)
 	#main([[1],[2],[3],[4],[5]], [['S','S','S','S','S'], ['S','S','S']], ['S'], [[1],[4],[9],[16],[25]], learnrate = 0.5, threshold = 5, momentum = 0.3)
 	#main([[1],[2],[3],[4],[5]], [['L', 'L', 'L']], ['S'], [[1],[4],[9],[16],[25]], learnrate = .5, threshold = 5, momentum = .3)
 	#main([[2,3], [1,3], [3,3]], [['G','G','G']], ['R'], [[101], [400], [3604]], learnrate = 0.5, threshold = 10, momentum = 0.5)
-	#main([[2,3], [1,3]], [['G','G','G']], ['R'], [[101], [400]], learnrate = 0.5, threshold = 10, momentum = 0.5)
+	main([[2,3], [1,3]], [['G','G','G']], ['R'], [[101], [400]], learnrate = 0.5, threshold = 10, momentum = 0.5)
 
