45b6dad9ce99b4819919d9d26bf556fe47cacf1d
Keydrain <clintgcooper@gmail.com>
2015-10-28 11:21:15 -0600

    DE Clean Conflicts
    Removed conflicting code in DE from recent merge.

--- Project3/DE.py
+++ Project3/DE.py
@@ -5,7 +5,6 @@ Author: 	Clint Cooper, Emily Rohrbough, Leah Thompson
 Date:   	10/25/15
 CSCI 447:	Project 3
 
-<<<<<<< HEAD
 Code for training a Neural Network via a differential evolution 
 algorithm - using the GA.py template for creating the neural 
 network, generating the population, evaluating the population and 
@@ -19,8 +18,6 @@ size, generation number, threshold, crossover rate, mutation rate)
 
 Output:
 Trained Neural Network.
-=======
->>>>>>> origin/master
 """
 
 import NN
@@ -34,21 +31,15 @@ from operator import sub
 OrigAnswers = []
 hero = 0
 
-<<<<<<< HEAD
 def crossover(donorV, trialV, cRate=.5):
     '''Binomial  Crossover: insures at least 1 element of the donor
     vector is carried forwared in the offspring. Otherwise, take the 
     next element from either the donor or trial vector if u(0,1) is 
     less than crossover rate.'''
-
-=======
-def crossover(donorV, trialV, cRate = .5):
->>>>>>> origin/master
     offspringV = []
     indices = 0
     if(len(donorV) == len(trialV)):
         indices = len(donorV)
-<<<<<<< HEAD
     forwardIndex = random.randint(0, indices-1)
     for i in range(indices):
         if random.random() < cRate and i != forwardIndex:
@@ -101,53 +92,10 @@ def train(inputs, outputs, size, generations, threshold, cRate, mRate):
     population = GA.generatePopulation(EvaluationNN, inputs, outputs, size)
 
     gen = 0
-=======
-    forwardIndex = random.random() * indices
-    offspringV.append(forwardIndex)
-    for i in range(indices):
-        if(i != forwardIndex):
-            if (random.random() * indices) < cRate:
-                offspringV.append(trialV[i])
-            else:
-                offspringV.append(donorV[i])
-    return offspringV
-
-def mutate(population, i, beta=.5): #several mutation schemas possible...
-    trialV = []
-    j = 0
-    k = 0
-    
-    same = 0
-    while(same > 2):
-        j = random.random() * len(population)
-        k = random.random() * len(population)
-        if j != i:
-            #vector2 = population[j]
-            same = same + 1
-        if k != i:
-            #vector3 = population[k]
-            same = same + 1
-    trialV.append(numpy.array(population[i]) - beta * (numpy.array(population[j]) - numpy.array(population[k])))
-        #(vector2[i] - vector3[i]))
-    return trialV
-
-def train(inputs, outputs, size, generations, cRate, mRate):
-    global hero
-    global OrigAnswers
-
-    EvaluationNN = GA.create_net(inputs, outputs)
-    #define an upper and lower bound. This is done in the NN.
-    population = []
-    population.append(GA.generatePopulation(EvaluationNN, inputs, outputs, size))
-   # population = GA.generatePopulation(EvaluationNN, inputs, outputs, size)
-    ####NEED TO FIGURE OUT THE DE VECTOR STRUCTURE SO I CAN CODE IT...
-    gen = 1 #should be 0...
->>>>>>> origin/master
     trialV = []
     offspringV = []
     # loop until a hero is found or we've reached max generations
     while gen <= generations: #and hero == 0:
-<<<<<<< HEAD
         # evaluate the entire population
         GA.evaluate(EvaluationNN, population, inputs, outputs)
 
@@ -163,7 +111,7 @@ def train(inputs, outputs, size, generations, cRate, mRate):
                population[i] = offspringV
 
         #check for hero in population
-        if GA.heroFound(population, threshold):
+        if GA.heroFound(population, len(inputs) * threshold):
             break
         print("Training {:2.2%}".format(gen / generations), end="\r")
         gen += 1
@@ -180,50 +128,13 @@ def train(inputs, outputs, size, generations, cRate, mRate):
         EvaluationNN.SetStartingNodesValues(x)
         EvaluationNN.CalculateNNOutputs()
         print(x, EvaluationNN.GetNNResults(), OrigAnswers[inputs.index(x)])
-=======
-                
-        for i in range(size):
-            print('Inside of loop...') 
-            # evaluate
-            GA.evaluate(EvaluationNN, population, inputs, outputs)
-            # mutate
-            #trialV.append(mutate(population, i, mRate))
-            # crossover
-            #crossover(population[i], trialV, cRate)
-            #selection
-            #GA.evaluate(EvaluationNN, offspringV, inputs, outputs)
-            #if(population[1][-1] < offspringV[-1]):
-            #    population[1] = offspringV
-
-            #if(GA.evaluate(EvaluationNN, offspringV, inputs, outputs) > GA.evaluate(EvaluationNN, population[i], inputs, outputs)):
-            #    population[i] = offspringV
-        gen += 1
-
-    # return best hero if max generations is met and hero hasn't been selected.
-    #hero = sorted(population, key=itemgetter(-1))[0]
-    #EvaluationNN.SetNNWeights(hero[:-1])  # Load hero into NN, prep for usage.
-    print()
-
-    # Evaluate the hero on the inputs and outputs
-    #for x in inputs:
-    #    EvaluationNN.SetStartingNodesValues(x)
-    #    EvaluationNN.CalculateNNOutputs()
-    #    print(gen, x, EvaluationNN.GetNNResults(),
-    #          OrigAnswers[inputs.index(x)])
->>>>>>> origin/master
 
     return EvaluationNN
 
 
-<<<<<<< HEAD
 def main(inputs, outputs, size=20, generations=100, threshold = 10,cRate=0.5, mRate=0.5):
 
     eval_nn = train(inputs, outputs, size, generations, threshold, cRate, mRate)
-=======
-def main(inputs, outputs, size=20, generations=100, cRate=0.5, mRate=0.5):
-
-    eval_nn = train(inputs, outputs, size, generations, cRate, mRate)
->>>>>>> origin/master
 
 if __name__ == '__main__':
     print('Starting some DE training...')
@@ -231,9 +142,6 @@ if __name__ == '__main__':
     #     generations=500, threshold=5)
     # main([[2, 3], [1, 3]], [[101], [400]], size=9, participants=6, victors=3,
     #     generations=100000, threshold=10, cRate=0.5, mRate=0.5)
-<<<<<<< HEAD
     main([[2, 3], [1, 3], [3, 3]], [[101], [400], [3604]], size=20, threshold=10, generations=100000, cRate=0.3, mRate=0.6)
-=======
-    main([[2, 3], [1, 3], [3, 3]], [[101], [400], [3604]], size=9,
-         generations=1, cRate=0.5, mRate=0.5)
->>>>>>> origin/master
+
+
