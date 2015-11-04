c0c7978e8d1dcf9401ae503e99fcaca790b8135c
lchristina26 <leah@wolfssl.com>
2015-11-04 13:38:19 -0700

    updates to handler - fully functional, multi outputs allowed

--- Project3/handler.py
+++ Project3/handler.py
@@ -10,7 +10,8 @@ import random
 cRate = 0.5
 mRate = 0.5
 threshold = 10
-generations = 100000
+generations = 10000
+num_outs = 1
 size = 20
 participants = 10
 victors = 5
@@ -25,11 +26,11 @@ def train_test():
     outputs = []
     evolve()
     
-    resultsFile.write("DATASET: " + dataset)
-    resultsFile.write("ALGORITHM | Generations | Size | Participants | Victors | mRate | cRate | Threshold")
+    resultsFile.write("DATASET: " + dataset + "\n")
+    resultsFile.write("ALGORITHM | Generations | Size | Participants | Victors | mRate | cRate | Threshold \n")
     resultsFile.write("   " + str(algo) + "      |     " + str(generations) + "      |  " +
               str(size) + "  |     " + str(participants) + "       |    " + str(victors) + 
-              "    |  " + str(mRate) + "  |  " + str(cRate) + "  |   " + str(threshold) + "     ")
+              "    |  " + str(mRate) + "  |  " + str(cRate) + "  |   " + str(threshold) + "     \n")
 
     dataIn = dataHandler()
     inputs = dataIn[0]
@@ -40,46 +41,49 @@ def train_test():
     momentum = 0.5
     # Need 20% of inputs for testing
     for i in range((int(len(inputs)*0.8)+1), len(inputs)):
-        testInput.append(random.choice(inputs))
-        testOutput.append(outputs[inputs.index(
-                                    testInput[i-((int(len(inputs)*0.8))+1)])])
-
+        x = random.choice(inputs)
+        testInput.append(x)
+        testOutput.append(outputs[inputs.index(x)])
+    resultsFile.write("\nTest inputs: \n")
+    for i in range(len(testInput)):
+        resultsFile.write("%s " % testInput[i])
+    resultsFile.write("\nTest expected outputs: \n")
+    for i in range(len(testOutput)):
+        resultsFile.write("%s " % testOutput[i])
     # Which algorithm gets chosen to run
     if algo in 'G':
-        resultsFile.write(testNN = GA.train(inputs, outputs, size, participants, 
-                         victors, generations, threshold, cRate, mRate))
+        print("DOING GA TRAINING")
+        testNN = GA.train(inputs, outputs, size, participants, 
+                         victors, generations, threshold, cRate, mRate)
     elif algo in 'E':
-        resultsFile.write(testNN = ES.train(inputs, outputs, size, participants, 
-                          victors, generations, threshold, cRate, mRate))
+        print("DOING ES TRAINING")
+        testNN = ES.train(inputs, outputs, size, participants, 
+                          victors, generations, threshold, cRate, mRate)
     elif algo in 'D':
-        resultsFile.write(testNN = DE.train(inputs, outputs, size, participants, 
-                          victors, generations, threshold, cRate, mRate))
+        print("DOING DE TRAINING")
+        testNN = DE.train(inputs, outputs, size, generations, threshold, 
+                                                                cRate, mRate)
     elif algo in 'B':
-        resultsFile.write(testNN = NN.main(inputs, [['S','S','S'], ['S','S']], 
-          ['S'], outputs, generations, learnrate, threshold, momentum))
+        testNN = NN.main(inputs, [['S','S','S'], ['S','S']], 
+          ['S'], outputs, generations, learnrate, threshold, momentum)
     else:
         print("Unrecognized algorithm!")
         sys.exit()
     # Print test input/expected output - could be made prettier in a table
-    resultsFile.write("Test inputs: ")
-    restulsFile.write(testInput)
-    resultsFile.write("Test expected outputs: ")
-    resultsFile.write(testOutput)
     # Start testing testNN
     for x in testInput:
-        resultsFile.write("Set starting node vals")
-        resultsFile.write(testNN.SetStartingNodesValues(x))
-        resultsFile.write("Calculate NN Outputs")
-        resultsFile.write(testNN.CalculateNNOutputs())
-        resultsFile.write("Test Input: " + str(x))
-        resultsFile.write("Test results: " + testNN.GetNNResults())
-    resultsFile.write("Relative error, least squares error:")
-    resultsFile.write(calcRelativeError(testNN, inputs, outputs))
-    resultsFile.write(calcLeastSqauresError(testNN, inputs, outputs))
-    close(resultsFile)
+        resultsFile.write("\nSet starting node vals\n")
+        resultsFile.write("%s \n" % testNN.SetStartingNodesValues(x))
+        resultsFile.write("\nCalculate NN Outputs\n")
+        resultsFile.write("%s \n" % testNN.CalculateNNOutputs())
+        resultsFile.write("\nTest Input: " + str(x) + "\n")
+        resultsFile.write("\nTest results: %s\n" % testNN.GetNNResults())
+    resultsFile.write("\nRelative Error: %s \n" % NN.calcRelativeError(testNN, inputs, outputs))
+    resultsFile.write("\nLeast Squares Error %s \n" % NN.calcLeastSquaresError(testNN, inputs, outputs))
+    resultsFile.close()
 
 def evolve():
-    global cRate, mRate, threshold, generations, size, participants, victors, inFile, algo, dataset, resultsFile
+    global cRate, mRate, num_outs, threshold, generations, size, participants, victors, inFile, algo, dataset, resultsFile
     data = False
     alg = False
 
@@ -88,21 +92,23 @@ def evolve():
             printHelp()
             sys.exit()
         elif sys.argv[i] in '-m':
-            mRate = sys.argv[i+1]
+            mRate = float(sys.argv[i+1])
         elif sys.argv[i] in '-i':
             inFile = sys.argv[i+1]
         elif sys.argv[i] in '-t':
-            threshold = sys.argv[i+1]
+            threshold = float(sys.argv[i+1])
         elif sys.argv[i] in '-c':
-            cRate = sys.argv[i+1]
+            cRate = float(sys.argv[i+1])
         elif sys.argv[i] in '-g':
-            generations = sys.argv[i+1]
+            generations = int(sys.argv[i+1])
         elif sys.argv[i] in '-s':
-            size = sys.argv[i+1]
+            size = int(sys.argv[i+1])
         elif sys.argv[i] in '-p':
-            participants = sys.argv[i+1]
+            participants = int(sys.argv[i+1])
         elif sys.argv[i] in '-v':
-            victors = sys.argv[i+1]
+            victors = int(sys.argv[i+1])
+        elif sys.argv[i] in '-o':
+            num_outs = int(sys.argv[i+1])
         elif sys.argv[i] in '-d':
             dataset = sys.argv[i+1]
             data = True
@@ -121,7 +127,7 @@ def evolve():
         sys.exit()
 
 def dataHandler():
-    global inFile
+    global inFile, num_outs
     index = 0
 
     with open(inFile, 'r') as f:
@@ -138,10 +144,11 @@ def dataHandler():
     temp = []
     for i in range(index+1, len(content)):
         ins.append(content[i].split())
-        temp.append(ins[i-(index+1)][0])
+        for j in range(num_outs):
+            temp.append(ins[i-(index+1)][0])
+            del ins[i-(index+1)][0]
         outs.append(temp)
         temp = []
-        del ins[i-(index+1)][0]
     del outs[0]
     for i in range(len(outs)):
         outs[i][0] = int(outs[i][0])
@@ -159,6 +166,7 @@ def printHelp():
     print ("-g <num>         set generations to num")
     print ("-v <num>         set victors to num")
     print ("-p <num>         set participants to num")
+    print ("-o <num>         set number of outputs per input set")
     print ("-a <algo>        G(GA), E(ES), D(DE), or B(Backprop)")
     print ("-h               print this help screen")
     print ("EXAMPLE CASE:")
