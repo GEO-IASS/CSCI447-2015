c87f4795095c20c8fadabff0c2c5c8b90baf902c
lchristina26 <leah@wolfssl.com>
2015-11-15 22:16:22 -0700

    update compLearn, meh

--- Project4/compLearn.py
+++ Project4/compLearn.py
@@ -40,18 +40,11 @@ def competitiveLearn(inputs, numHNodes, iterations, learnRate):
     wcount = 0
     temp_weights = []
     tmp_wt = []
-##    for x in inputs:
-##        x = numpy.linalg.norm(x) # normalize inputs
-##        print("Normalized input vector: ", x)
-#        weights.append(random.random())
-#        weights[wcount] = numpy.linalg.norm(weights[wcount])
-#        wcount+=1
     # randomly assign numHNodes input vectors to be the weights
     for i in range(numHNodes):
         weights.append(random.choice(inputs))
         weights[i] = numpy.linalg.norm(weights[i])
     temp_weights = weights
-##        weights[i] = numpy.linalg.norm(weights[i])
 
     for i in range(iterations):
         # randomly select an input vector for comparison
@@ -61,13 +54,15 @@ def competitiveLearn(inputs, numHNodes, iterations, learnRate):
         # calculate a starting point for the winner
         winner = weights[0] * selectedInput
         # find the winning weight vector
+        index = 0
         for w in weights:
-            index = weights.index(w)
             tmp_w = w
-            temp = tmp_w*selectedInput
-            if winner >= temp:
+            temp = tmp_w * selectedInput
+            if winner <= temp:
                 winner = temp
-        weights[index] = learnRate*(math.fabs(selectedInput - weights[index]))
+                index = weights.index(w)
+        weights[index] = weights[index] + learnRate*(selectedInput - 
+                                                                weights[index])
     temp_inputs = []
     tmp_i = []
     # normalize inputs to compare against weights
@@ -89,4 +84,5 @@ def main(inputs, numHNodes, iterations, learnRate):
 if __name__ == '__main__':
 #    main([[10,10,7,8],[25,20,24,25],[1,1,1,1],[3,3,3,3],[2,2,2,2],[1,1,1,1],
 #         [3,3,3,3]], 2, 500, 0.2)
-    main([[300,200],[20,10],[50,70],[5,1]], 4, 5000, 0.2)
+    main([[10,10],[2,2],[10,10],[2,2],[10,10],[2,2],[10,10],[2,2],[10,10],[2,2],[10,10],[2,2]], 2, 1000, 0.25)
+
